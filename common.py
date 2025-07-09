fa7b3015207dd78935ee9870a0909f3b30d7655d
daniil-zahoruiko <daniel.zagoruyko@gmail.com>
2025-07-08 23:57:33 -0600

    Change secrets extension

--- common.py
+++ common.py
@@ -1,8 +1,10 @@
 import json
 import requests
 
+FILENAME = 'secrets.json'
+
 def load_api_key():
-    with open('secrets.txt') as f:
+    with open(FILENAME) as f:
         return json.load(f)['API_KEY']
     
 def refresh_token(url, api_key):
